292e1fc86586b292b22bedeec3408e34b1281b5c
pjpakash <blitzbeats45@gmail.com>
2025-10-25 05:36:18 +0530

    Update settings.py

--- ShrutiMusic/utils/inline/settings.py
+++ ShrutiMusic/utils/inline/settings.py
@@ -1,25 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
 from typing import Union
 
 from pyrogram.types import InlineKeyboardButton
@@ -134,13 +112,13 @@ def playmode_users_markup(
     return buttons
 
 
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+# ©️ Copyright Reserved - @ZoxxOP  Akash Dakshwanshi
 
 # ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ©️ 2025 Akash Dakshwanshi (aka @ZoxxOP)
+# 🔗 GitHub : https://github.com/ZoxxOP/AnanyaMusic
+# 📢 Telegram Channel : https://t.me/AnanyaBots
 # ===========================================
 
 
-# ❤️ Love From ShrutiBots 
+# ❤️ Love From AnanyaBots 
